a1faeca4fe3996b4cadc2c1d40672cfb0107444b
Quan Hoang <quanhuhoq111@gmail.com>
2021-08-23 15:19:48 +0700

    upload file quyetdinh

--- app/Models/QuyetDinh.php
+++ app/Models/QuyetDinh.php
@@ -10,110 +10,53 @@ class QuyetDinh extends Model
 {
     use HasFactory;
     public $timestamps = true;
+    protected $table='quyet_dinhs';
 
-    /**
-     * Get the phongban for the nhansu.
-     */
-    public function phongbans()
+
+    public function scopeGetByNhanSuId($query, $id)
     {
-        return $this->belongsTo('App\PhongBan', 'phongban_id');
+        return $query->where('nhansu_id', $id);
     }
 
     /**
-     * Get the bophan for the nhansu.
+     * Get the loaiquyetdinh for the quyetdinh.
      */
-    public function bophans()
+    public function loaiquyetdinhs()
     {
-        return $this->belongsTo('App\BoPhan', 'bophan_id');
+        return $this->belongsTo('App\LoaiQuyetDinh', 'loaiquyetdinh_id');
     }
 
-    /**
-     * Save nhân sự
-     * -1  : Import
-     *  0  : Add
-     * $id : Edit
-     */
-    public static function saveNhanSu($id, $data){
-        if($id == 0 || $id == -1){
-            $nhan_su = new NhanSu;
+    public static function saveQuyetDinh($id, $data){
+        if($id == 0){
+            $quyet_dinh = new QuyetDinh;
         }else{
-            $nhan_su = NhanSu::findOrFail($id);
-            $nhan_su->trang_thai     = $data['trang_thai'];
+            $quyet_dinh = QuyetDinh::findOrFail($id);
         }
-        $nhan_su->ma_nv              = $data['ma_nv'];
-        $nhan_su->ho_ten             = $data['ho_ten'];
-        $nhan_su->dia_chi_thuong_tru = $data['dia_chi_thuong_tru'];
-        $nhan_su->dia_chi_lien_he    = $data['dia_chi_lien_he'];
-        $nhan_su->dien_thoai         = $data['dien_thoai'];
-        $nhan_su->email              = $data['email'];
-        $nhan_su->gioi_tinh          = $data['gioi_tinh'];
-        $nhan_su->ngay_sinh          = Carbon::parse($data['ngay_sinh'])->format('Y-m-d');
-        $nhan_su->so_cmnd            = $data['so_cmnd'];
-        if($data['ngay_cap_cmnd'] != null){
-            $nhan_su->ngay_cap_cmnd      = Carbon::parse($data['ngay_cap_cmnd'])->format('Y-m-d');
-        }else{
-            $nhan_su->ngay_cap_cmnd      = null;
-        }
-        
-        $nhan_su->noi_cap_cmnd       = $data['noi_cap_cmnd'];
-        if($data['ngay_bat_dau_lam'] != null){
-            $nhan_su->ngay_bat_dau_lam   = Carbon::parse($data['ngay_bat_dau_lam'])->format('Y-m-d');
-        }else{
-            $nhan_su->ngay_bat_dau_lam   = null;
-        }
-        
-        if($data['ngay_lam_viec_cuoi'] != null){
-            $nhan_su->ngay_lam_viec_cuoi   = Carbon::parse($data['ngay_lam_viec_cuoi'])->format('Y-m-d');
-        }else{
-            $nhan_su->ngay_lam_viec_cuoi   = null;
-        }
-        $nhan_su->trinh_do           = $data['trinh_do'];
-        $nhan_su->truong_tot_nghiep  = $data['truong_tot_nghiep'];
-        $nhan_su->nam_tot_nghiep     = $data['nam_tot_nghiep'];
-        $nhan_su->chung_chi          = $data['chung_chi'];
-        $nhan_su->chuc_danh          = $data['chuc_danh'];
-        $nhan_su->phongban_id        = $data['phongban_id'];
-        $nhan_su->bophan_id          = $data['bophan_id'];
-        if($id == -1){
-            $nhan_su->hoso_id        = $data['hoso_id'];
-        }else{
-            if(!empty($data['hoso_id'])){
-                $nhan_su->hoso_id        = implode(',', $data['hoso_id']);
-            }else{
-                $nhan_su->hoso_id        = null;
-            }
-        }
-        
-        $nhan_su->save();
-        return $nhan_su;
+        $quyet_dinh->nhansu_id            = $data['nhansu_id'];
+        $quyet_dinh->ma_qd                = $data['ma_qd'];
+        $quyet_dinh->loaiquyetdinh_id     = $data['loaiquyetdinh_id'];
+        $quyet_dinh->ngay_ky              = Carbon::parse($data['ngay_ky_qd'])->format('Y-m-d');
+        $quyet_dinh->can_cu               = $data['can_cu'];
+        $quyet_dinh->noi_nhan             = $data['noi_nhan'];
+        $quyet_dinh->tong_thu_nhap_cu     = $data['tong_thu_nhap_cu'];
+        $quyet_dinh->tong_thu_nhap_moi    = $data['tong_thu_nhap_moi'];
+        $quyet_dinh->luong_co_ban_moi     = $data['luong_co_ban_moi'];
+        $quyet_dinh->luong_tro_cap_moi    = $data['luong_tro_cap_moi'];
+        $quyet_dinh->luong_hieu_qua_moi   = $data['luong_hieu_qua_moi'];
+        $quyet_dinh->ly_do                = $data['ly_do'];
+        $quyet_dinh->chuc_vu_cu           = $data['chuc_vu_cu'];
+        $quyet_dinh->chuc_vu_moi          = $data['chuc_vu_moi'];
+        $quyet_dinh->bo_phan_cu           = $data['bo_phan_cu'];
+        $quyet_dinh->bo_phan_moi          = $data['bo_phan_moi'];
+        $quyet_dinh->chuc_vu_hien_tai     = $data['chuc_vu_hien_tai'];
+        $quyet_dinh->trang_thai           = $data['trang_thai'];
+        // dd($quyet_dinh);
+        $quyet_dinh->save();
+        return $quyet_dinh;
     }
 
-    public function getNgaySinhAttribute($value)
+    public function getNgayKyAttribute($value)
     {
         return Carbon::parse($value)->format('d-m-Y');
-    }
-
-    public function getNgayCapCmndAttribute($value)
-    {
-        if($value != null){
-            return Carbon::parse($value)->format('d-m-Y');
-        }
-        return null;
-    }
-
-    public function getNgayBatDauLamAttribute($value)
-    {
-        if($value != null){
-            return Carbon::parse($value)->format('d-m-Y');
-        }
-        return null;
-    }
-
-    public function getNgayLamViecCuoiAttribute($value)
-    {
-        if($value != null){
-            return Carbon::parse($value)->format('d-m-Y');
-        }
-        return null;
     }
 }
